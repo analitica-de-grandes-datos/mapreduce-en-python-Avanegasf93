ebed8729d1cddd03625953f782544fef6df98927
Andrés Vanegas <112149548+Avanegasf93@users.noreply.github.com>
2023-06-15 22:39:39 -0500

    CommitP1.1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys  # Importa el módulo 'sys' para trabajar con la entrada estándar
+
+def purpose_amount(x):
+    return x[2]  # Devuelve el tercer elemento de la lista 'x'
+
+for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+    result = line.split(',')  # Divide la línea en una lista utilizando ',' como separador
+    print(purpose_amount(result))  # Imprime el tercer elemento de la lista 'result'
+
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys  # Importa el módulo 'sys' para trabajar con la entrada estándar
+biggest_purposes = {}  # Crea un diccionario vacío llamado 'biggest_purposes' para almacenar los propósitos más grandes
+
+def set_bigger_purpose(dictionary_purposes, actual_element):
+    # Incrementa el contador para el elemento actual en el diccionario de propósitos más grandes
+    dictionary_purposes[actual_element] = int(dictionary_purposes.get(actual_element) or 0) + 1
+    return dictionary_purposes  # Devuelve el diccionario actualizado de propósitos más grandes
+
+for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+    set_bigger_purpose(biggest_purposes, line.replace("\n", ""))  # Llama a la función para establecer el propósito más grande
+
+for purpose, amount in biggest_purposes.items():  # Itera sobre cada par de clave-valor en el diccionario de propósitos más grandes
+    print(purpose + "    " + str(amount))  # Imprime el propósito y la cantidad correspondiente
+
