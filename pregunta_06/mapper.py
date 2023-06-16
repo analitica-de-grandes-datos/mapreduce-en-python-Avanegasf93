68dcbf55aba9f6dcc11db4b1290d1034e424f22b
Andrés Vanegas <112149548+Avanegasf93@users.noreply.github.com>
2023-06-15 23:53:18 -0500

    CommitP6.2

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,27 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-import sys  # Importa el módulo 'sys' para trabajar con la entrada y salida estándar
+import sys
 
-if __name__ == '__main__':  # Comprueba si el script se ejecuta directamente (no como módulo)
-
-    curkey = None  # Variable para almacenar la clave actual
-    total = 0  # Variable para almacenar el total acumulado
-
-    for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
-
-        key, val = line.split("\t")  # Divide la línea en clave y valor utilizando el tabulador como separador
-        val = int(val)  # Convierte el valor a entero
-
-        if key == curkey:  # Si la clave actual es igual a la clave procesada anteriormente
-            total += val  # Incrementa el total acumulado con el valor actual
-        else:
-            if curkey is not None:  # Si la clave actual no es None (es decir, no es la primera línea)
-                # Escribe en la salida estándar la clave anterior y el total acumulado
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
-
-            curkey = key  # Actualiza la clave actual con la nueva clave
-            total = val  # Reinicia el total acumulado con el nuevo valor
-
-    # Escribe en la salida estándar la última clave y el total acumulado
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+if __name__ == '__main__':
+    # Iterar sobre cada línea de entrada en la entrada estándar
+    for line in sys.stdin:
+        # Eliminar los espacios en blanco al inicio y al final, y reemplazar el guión por un espacio
+        lista = line.strip().replace('-', ' ').split(' ')
+        
+        # Escribir el primer y octavo elemento de la lista separados por un tabulador en la salida estándar
+        sys.stdout.write("{}\t{}\n".format(lista[0], lista[8]))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,27 +1,40 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-import sys  # Importa el módulo 'sys' para trabajar con la entrada y salida estándar
+import sys
 
-if __name__ == '__main__':  # Comprueba si el script se ejecuta directamente (no como módulo)
+if __name__ == '__main__':
+    curkey = None
+    max_val = 0
+    min_val = 0
 
-    curkey = None  # Variable para almacenar la clave actual
-    total = 0  # Variable para almacenar el total acumulado
+    for line in sys.stdin:
+        # Divide la línea en dos partes utilizando el carácter de tabulación como separador
+        row = line.split("\t")
+        key = row[0]
+        val = float(row[1])
 
-    for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+        if key == curkey:
+            # Compara el valor actual con el máximo y mínimo actuales
+            if val > max_val:
+                max_val = val
+            else:
+                val = max_val
 
-        key, val = line.split("\t")  # Divide la línea en clave y valor utilizando el tabulador como separador
-        val = int(val)  # Convierte el valor a entero
+            if val < min_val:
+                min_val = val
+            else:
+                val = min_val
 
-        if key == curkey:  # Si la clave actual es igual a la clave procesada anteriormente
-            total += val  # Incrementa el total acumulado con el valor actual
         else:
-            if curkey is not None:  # Si la clave actual no es None (es decir, no es la primera línea)
-                # Escribe en la salida estándar la clave anterior y el total acumulado
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
+            # Si la clave cambia, escribe la clave anterior junto con los valores máximo y mínimo en la salida estándar
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, max_val, min_val))
 
-            curkey = key  # Actualiza la clave actual con la nueva clave
-            total = val  # Reinicia el total acumulado con el nuevo valor
+            # Actualiza la clave y los valores máximo y mínimo para la nueva clave
+            curkey = key
+            max_val = val
+            min_val = val
 
-    # Escribe en la salida estándar la última clave y el total acumulado
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+    # Escribe la última clave junto con los valores máximo y mínimo en la salida estándar
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, max_val, min_val))
