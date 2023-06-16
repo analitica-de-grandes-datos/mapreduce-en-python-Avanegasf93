180c856185dedfb27c0b623dc4ee5814a6469e94
Andrés Vanegas <112149548+Avanegasf93@users.noreply.github.com>
2023-06-15 22:52:36 -0500

    CommitP3.1

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys  # Importa el módulo 'sys' para trabajar con la entrada estándar
+
+def clear_spaces(x):
+    x = x.replace("\n", "")  # Elimina los saltos de línea
+    x = x.replace("\r", "")  # Elimina los retornos de carro
+    return x  # Devuelve la cadena 'x' sin espacios adicionales
+
+def purpose_amount(x):
+    return clear_spaces(x[1]) + "*" + clear_spaces(x[0])  # Concatena el segundo elemento de la lista 'x', un asterisco y el primer elemento
+
+for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+    result = line.split(',')  # Divide la línea en una lista utilizando ',' como separador
+    print(purpose_amount(result))  # Imprime el resultado de la función 'purpose_amount' aplicada a la lista 'result'
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys  # Importa el módulo 'sys' para trabajar con la entrada estándar
+
+for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+    line = line.replace("\n", "")  # Elimina los saltos de línea de la línea actual
+    if '*' in line:  # Verifica si la línea contiene un asterisco
+        # Imprime la línea dividida en dos partes intercambiadas utilizando el asterisco como separador
+        print(line.split("*")[1] + "," + line.split("*")[0])
