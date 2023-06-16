5e81517f876d9ee37443814221562ad3bd734d89
Andrés Vanegas <112149548+Avanegasf93@users.noreply.github.com>
2023-06-15 22:50:28 -0500

    CommitP2.1

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys  # Importa el módulo 'sys' para trabajar con la entrada estándar
+
+def purpose_amount(x):
+    return x[3] + "*" + x[4]  # Concatena el cuarto elemento de la lista 'x', un asterisco y el quinto elemento
+
+for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+    result = line.split(',')  # Divide la línea en una lista utilizando ',' como separador
+    print(purpose_amount(result))  # Imprime el resultado de la función 'purpose_amount' aplicada a la lista 'result'
--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys  # Importa el módulo 'sys' para trabajar con la entrada estándar
+biggest_purposes = {}  # Crea un diccionario vacío llamado 'biggest_purposes' para almacenar los propósitos más grandes
+
+def set_bigger_purpose(dictionary_purposes, actual_element):
+    element_array = actual_element.split("*")  # Divide el elemento actual en una lista utilizando '*' como separador
+    # Establece el valor más grande entre el valor actual del propósito en el diccionario y el valor en la posición 1 de la lista
+    dictionary_purposes[element_array[0]] = max(
+        int(dictionary_purposes.get(element_array[0]) or 0), int(element_array[1]))
+    return dictionary_purposes  # Devuelve el diccionario actualizado de propósitos más grandes
+
+for line in sys.stdin:  # Itera sobre cada línea de la entrada estándar
+    set_bigger_purpose(biggest_purposes, line)  # Llama a la función para establecer el propósito más grande
+
+for purpose, amount in biggest_purposes.items():  # Itera sobre cada par de clave-valor en el diccionario de propósitos más grandes
+    print(purpose + "    " + str(amount))  # Imprime el propósito y la cantidad correspondiente
